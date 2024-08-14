6e0941670be723f35cce622b63ff1db840c09450
Takashi Kakinari <tkakinari@hotmail.com>
2024-08-14 14:12:32 +0900

    version up and configuration change
    version up and configuration change

--- ubi-micro-node/build-ubi-micro-node.sh
+++ ubi-micro-node/build-ubi-micro-node.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-VERSION=22.4.1
+VERSION=22.6.0
 
 podman build --build-arg=VERSION=22 -t docker.io/kakinari/ubi-micro-ja:9-node-latest .
 podman tag  docker.io/kakinari/ubi-micro-ja:9-node-latest docker.io/kakinari/ubi-micro-ja:9-node-${VERSION}
