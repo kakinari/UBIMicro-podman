c74a20aef9d850718709097441dee149cbd5d1e0
Takashi Kakinari <tkakinari@hotmail.com>
2026-06-01 03:03:49 +0900

    new codespace java versions

---
+++ ubi-micro-cs-java/build-ubi-micro-jbase.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+VERSION=${1:-latest}
+microcontainer=$(buildah from docker.io/kakinari/ubi-micro-ja:10-csbase)
+micromount=$(buildah mount $microcontainer)
+dnf install \
+--installroot $micromount \
+--releasever=/ \
+--setopt install_weak_deps=false \
+--setopt=reposdir=/etc/yum.repos.d/ \
+--nodocs -y \
+java-${VERSION}-openjdk \
+java-${VERSION}-openjdk-devel \
+java-${VERSION}-openjdk-jmods
+
+buildah umount $microcontainer
+buildah commit $microcontainer localhost/kakinari/ubi-micro-ja:10-cs-java
+buildah rm $microcontainer
+podman build --build-arg VERSION=${VERSION}  -t docker.io/kakinari/ubi-micro-ja:10-cs-java-${VERSION} .
+podman push docker.io/kakinari/ubi-micro-ja:10-cs-java-${VERSION}
+podman rmi -f localhost/kakinari/ubi-micro-ja:10-cs-java
+podman rmi -f docker.io/kakinari/ubi-micro-ja:10-cs-java-${VERSION}
+podman rmi -f docker.io/kakinari/ubi-micro-ja:10-csbase
